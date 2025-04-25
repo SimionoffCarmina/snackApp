f0adc0a32105902c35bc9480082527e502fead06
Simionoff Carmina <cb.simionoff@gmail.com>
2025-04-25 16:33:53 +0300

    changed unit nullable status to true

--- api/models/ingredient.py
+++ api/models/ingredient.py
@@ -6,9 +6,9 @@ class Ingredient(db.Model):
     __tablename__ = 'ingredient'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), primary_key=True)
-    unit = db.Column(db.String(50), primary_key=True)
-    quantity = db.Column(db.Float, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    unit = db.Column(db.String(50),  nullable=True)
+    quantity = db.Column(db.Float,  nullable=False)
 
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
     recipe = db.relationship('Recipe', backref=db.backref('ingredients', lazy=True))
