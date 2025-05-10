c6804aaa2a7446586ef824531584e8e228fc732f
Simionoff Carmina <cb.simionoff@gmail.com>
2025-05-10 13:13:04 +0300

    Updated create recipe and delete recipe functions to integrate the database

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 
@@ -10,10 +11,23 @@ from models.recipe import Recipe
 from models.ingredient import Ingredient
 from models.association import recipe_category
 
+CATEGORY_COLORS = {
+    'Baking': '#FFA500',  # orange
+    'Cookies': '#D2691E',  # chocolate
+    'Pie': '#FFD700',  # gold
+    'Italian': '#FF0000',  # red
+    'No-bake': '#ADD8E6'  # lightblue
+}
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+def get_recipe_as_list():
+    recipes = []
+    for recipe in db.session.query(Recipe).all():
+        recipes.append(recipe.as_dict())
+    return recipes
 @app.route('/')
 def hello_world():
     return 'Welcome to SnackApp!'
@@ -21,17 +35,13 @@ def hello_world():
 
 @app.route('/api/recipes', methods=['GET'])
 def get_recipes():
-    recipes = []
-    for recipe in db.session.query(Recipe).all():
-        recipes.append(recipe.as_dict())
+    recipes = get_recipe_as_list()
     return jsonify(recipes)
 
 
 @app.route('/api/recipes/<int:recipe_id>', methods=['GET'])
 def get_recipe(recipe_id):
-    recipes = []
-    for recipe in db.session.query(Recipe).all():
-        recipes.append(recipe.as_dict())
+    recipes = get_recipe_as_list()
     for recipe in recipes:
         if recipe['id'] == recipe_id:
             return jsonify(recipe)
@@ -40,37 +50,69 @@ def get_recipe(recipe_id):
 
 @app.route('/api/recipes', methods=['POST'])
 def create_recipe():
-    new_recipe = {
-        'id': len(recipes) + 1,
-        'name': request.json.get('name'),
-        'duration': request.json.get('duration'),
-        'pictures': request.json.get('pictures'),
-        'instructions': request.json.get('instructions'),
-        'categories': request.json.get('categories'),
-        'ingredients': request.json.get('ingredients'),
-    }
-    recipes.append(new_recipe)
-    return jsonify(new_recipe)
+    data = request.get_json()
+    if data is None:
+        return jsonify({'message': 'No input data provided'}), 400
 
-@app.route('/api/recipes/<int:recipe_id>', methods=['PUT'])
-def update_recipe(recipe_id):
-    for recipe in recipes:
-        if(recipe['id'] == recipe_id):
-            recipe['name'] = name if( name:= request.json.get('name')) else recipe['name']
-            recipe['duration'] = duration if(duration:= request.json.get('duration')) else recipe['duration']
-            recipe['pictures'] = pictures if(pictures:= request.json.get('pictures')) else recipe['pictures']
-            recipe['instructions'] = instructions if(instructions:= request.json.get('instructions')) else recipe['instructions']
-            recipe['categories'] = categories if(categories:= request.json.get('categories')) else recipe['categories']
-            recipe['ingredients'] = ingredients if(ingredients:= request.json.get('ingredients')) else recipe['ingredients']
-    return recipes[recipe_id - 1]
+    categories = []
+    for name in data.get('categories', []):
+        cat = Category.query.filter_by(name=name).first()
+        if not cat:
+            cat = Category(name=name)
+            db.session.add(cat)
+        categories.append(cat)
+
+    ingredient_regex = re.compile(r'^(?P<quantity>\d+)(?P<unit>[a-zA-Z]*) (?P<name>.+)$')
+    ingredients = []
+    ingredients_data = data.get('ingredients', [])
+    for ingredient_str in ingredients_data:
+        match = ingredient_regex.match(ingredient_str.strip())
+        if not match:
+            return jsonify({'error': f'Invalid ingredient format: {ingredient_str}'}), 400
+
+        ingredient = Ingredient(
+            name=match['name'],
+            unit=match['unit'] or None,
+            quantity=float(match['quantity'])
+        )
+        ingredients.append(ingredient)
+
+
+    new_recipe = Recipe(
+        name = data.get('name'),
+        duration = data.get('duration'),
+        pictures = data.get('pictures'),
+        instructions = data.get('instructions'),
+        categories = categories,
+        ingredients = ingredients
+    )
+
+    db.session.add(new_recipe)
+    db.session.flush()
+
+    for ing in ingredients:
+        ing.recipe_id = new_recipe.id
+        db.session.add(ing)
+
+    db.session.commit()
+    return "added recipe"
+
 
 @app.route('/api/recipes/<int:recipe_id>', methods=['DELETE'])
 def delete_recipe(recipe_id):
-    for recipe in recipes:
-        if(recipe['id'] == recipe_id):
-            recipes.remove(recipe)
-            return 'deleted recipe', 200
-    return 'Recipe not found', 404
+    recipe = Recipe.query.get(recipe_id)
+
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+
+    for ingredient in recipe.ingredients:
+        db.session.delete(ingredient)
+
+    db.session.delete(recipe)
+    db.session.commit()
+
+    return jsonify({'message': 'Recipe deleted'}), 200
+
 
 if __name__ == '__main__':
     with app.app_context():
