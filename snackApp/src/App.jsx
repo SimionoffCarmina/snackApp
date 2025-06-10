8f91bc3d265dfe841866f6cc318a7e09038e630b
Simionoff Carmina <cb.simionoff@gmail.com>
2025-06-10 18:24:49 +0300

    added the addrecipeform, cat select, and recipepage files

--- snackApp/src/App.jsx
+++ snackApp/src/App.jsx
@@ -1,30 +1,41 @@
-import {useState, useEffect} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
-import {HomePage} from './pages/HomePage.jsx'
-import {NavBar} from './components/NavBar.jsx'
-import {AddCategoryModal} from "./components/AddCategoryModal.jsx";
+import { Navbar } from "./components/Navbar.jsx"
+import { HomePage } from './pages/HomePage.jsx'
+import { RecipePage } from './pages/RecipePage.jsx'
+import { AddRecipeModal } from './components/AddRecipeForm.jsx'
+import { AddCategoryModal } from './components/AddCategoryModal.jsx'
 
 function App() {
-    const [showCategoryModal, setShowCategoryModal] = useState(false)
-    return (
-        <>
-            <NavBar
-                handleOpenCategoryModal = {() => setShowCategoryModal(true)}
-            />
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path='/'
-                        element={<HomePage/>}
-                    />
-                </Routes>
-            </BrowserRouter>
-            <AddCategoryModal show={showCategoryModal} handleClose={() => { setShowCategoryModal(false)}}/>
-        </>
-    )
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showAddCategoryModal, setShowAddCategoryModal] = useState(false);
+
+  return (
+    <>
+      <Navbar
+        onAddRecipe={() => setShowAddModal(true)}
+        onAddCategory={() => setShowAddCategoryModal(true)}
+      />
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='/recipes/:recipeId'
+            element={<RecipePage />}
+          />
+        </Routes>
+      </BrowserRouter>
+
+      <AddRecipeModal showAddModal={showAddModal} setShowAddModal={setShowAddModal} />
+      <AddCategoryModal show={showAddCategoryModal}   handleClose={() => setShowAddCategoryModal(false)}
+ />
+    </>
+  )
 }
 
 export default App
